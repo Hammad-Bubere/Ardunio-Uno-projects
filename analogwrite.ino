e3f45c25d7bce352329f71fca03f364249b8d477
HammadBubere <76731254+HammadBubere@users.noreply.github.com>
2021-01-23 11:51:38 +0530

    Add files via upload

---
+++ analogwrite.ino
@@ -0,0 +1,9 @@
+//in analog write we use pwm pins(3,5,6,10,11) and there is no need to define pin
+void setup() {
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+analogWrite(3, 255);
+}
